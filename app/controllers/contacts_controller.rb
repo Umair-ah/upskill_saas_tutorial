200f877320468e2f57596f567ee74e7236a9e56b
Umair Ahmed <umairahmed200203.umair@gmail.com>
2022-10-05 10:01:38 +0000

    Added Create action to save into db'

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,24 @@
 class ContactsController < ApplicationController
     
     def new
+        @Contact = Contact.new
         
     end
     
+    def create
+        @Contact = Contact.new(contact_params)
+        
+        if @Contact.save
+            redirect_to new_contact_path, notice: 'Message Sent.'
+            
+        else
+            redirect_to new_contact_path, notice: 'Error Occured.'
+            
+        end
+    end
     
-    
+    private
+        def contact_params
+            params.require(:contact).permit(:Name, :Email, :Comments)
+        end
 end
